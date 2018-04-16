0a041733bc35c0cf337c5e886667cad3f8f2b803
Maikel Stuart <nigelzin.eduardo@gmail.com>
2018-04-16 17:37:29 -0300

    0.2

--- src/utils/api.js
+++ src/utils/api.js
@@ -9,3 +9,45 @@ const headers = {
   'Accept': 'application/json',
   'Authorization': token
 }
+/* Get - API*/
+
+/* Get all of the categories available for the app. */
+export const getAllCategories = () =>
+  fetch(`${api}/categories`, { headers })
+    .then(res => res.json())
+    .then(data => data.categories)
+/* Get all of the posts for a particular category. */
+export const getAllPostsCategory = (category) =>
+  fetch(`${api}/${category}/posts`, { headers })
+    .then(res => res.json())
+    .then(data => data.categoryPosts)
+/* Get all of the posts. Useful for the main page when no category is selected. */
+export const getAllPosts = () =>
+  fetch(`${api}/posts`, { headers })
+    .then(res => res.json())
+    .then(data => data.posts)
+/* 	Get the details of a single post. */
+export const getPost = (id) =>
+  fetch(`${api}/posts/${id}`, { headers })
+    .then(res => res.json())
+    .then(data => data.post)
+/* Get all the comments for a single post. */
+export const getPostComments = (id) =>
+  fetch(`${api}/posts/${id}/comments`, { headers })
+    .then(res => res.json())
+    .then(data => data.post)
+/* 	Get the details for a single comment. */
+export const getComment = (id) =>
+  fetch(`${api}/comments/${id}`, { headers })
+    .then(res => res.json())
+    .then(data => data.post)
+
+export const update = (book, shelf) =>
+    fetch(`${api}/books/${book.id}`, {
+    method: 'PUT',
+    headers: {
+      ...headers,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ shelf })
+  }).then(res => res.json())
