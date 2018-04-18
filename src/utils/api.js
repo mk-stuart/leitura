c4e0a669a73d3183595f5668819dcd0bd2422ca2
Maikel Stuart <nigelzin.eduardo@gmail.com>
2018-04-18 17:27:08 -0300

    0.5

--- src/utils/api.js
+++ src/utils/api.js
@@ -19,39 +19,30 @@ export const getAllCategories = () =>
   fetch(`${api}/categories`, { headers })
     .then(res => res.json())
     .then(data => data.categories)
+
 /* Get all of the posts for a particular category. */
 export const getAllPostsCategory = (category) =>
   fetch(`${api}/${category}/posts`, { headers })
     .then(res => res.json())
     .then(data => data.categoryPosts)
+
 /* Get all of the posts. Useful for the main page when no category is selected. */
 export const getAllPosts = () =>
   fetch(`${api}/posts`, { headers })
     .then(res => res.json())
-    //.then(data => data.posts)
 
-export function fetchPosts () {
-  
-    /*var request = new Request('http://localhost:3001/posts', {
-	    headers: new Headers({
-		    'Authorization': 'whatever'
-      })
-    });
-
-    return fetch(request)
-        .then((res) => res.json())*/
-    
-}    
 /* 	Get the details of a single post. */
 export const getPost = (id) =>
   fetch(`${api}/posts/${id}`, { headers })
     .then(res => res.json())
     .then(data => data.post)
+
 /* Get all the comments for a single post. */
 export const getPostComments = (id) =>
   fetch(`${api}/posts/${id}/comments`, { headers })
     .then(res => res.json())
     .then(data => data.post)
+    
 /* 	Get the details for a single comment. */
 export const getComment = (id) =>
   fetch(`${api}/comments/${id}`, { headers })
